c2a10cb92a209f55bc533689b55b3c0add637acc
Korng Samnang <korngsamnang168@gmail.com>
2024-10-04 22:46:59 +0700

    Make a bug

--- client/app/_components/sidebar.tsx
+++ client/app/_components/sidebar.tsx
@@ -96,7 +96,7 @@ const Sidebar = () => {
                         isSidebarCollapsed ? "hidden" : "block"
                     } font-extrabold text-2xl`}
                 >
-                    EDSTOCK
+                    EDSTOCKK
                 </h1>
 
                 <button
